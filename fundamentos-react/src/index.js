143aa0c00b0b8c8264748cd024f35d7f518261d9
matheus souza <ruivorobado@gmail.com>
2021-02-13 20:48:55 -0300

    adicionando o import do componente Primeiro que esta exportando uma function q retorna uma string

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,7 +2,9 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './components/Primeiro.jsx' //importando o componente primeiro para renderizar
+
 ReactDOM.render( 
-    <h1> Ola, mundo! </h1>,
+    <Primeiro></Primeiro>,
     document.getElementById('root')
 )
