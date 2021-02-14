2f975207ef117bcd40196e437aecf6ef8148ce81
matheus souza <ruivorobado@gmail.com>
2021-02-14 00:32:24 -0300

    retirando conteudos pra serem renderizados do index e conlocando no app.jsx

---
+++ fundamentos-react/src/components/layout/app.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ComParametros from './components/layout/ComParametros.jsx'
+import Primeiro from './components/layout/Primeiro.jsx' //importando o componente primeiro para renderizar
+import ComFilhos from './components/layout/ComFilhos'
+import Card from './components/layout/Card.jsx'
+
+export default props =>(
+  
+<div>
+    <Card titulo="exercicio x">
+        <Primeiro />    
+    </Card>
+{/*
+    <ComFilhos>
+        <ul>
+            <li>frase 1</li>
+            <li>frase 2</li>
+            <li>frase 3</li>
+        </ul>
+</ComFilhos>
+
+    <Primeiro />
+    <ComParametros titulo="esse é o titulo" subtitulo="esse é o subtitulo" />
+    <ComParametros titulo="opa" subtitulo="epa" />
+    //titulo e subtitulo sao parametros dos componentes
+*/
+}
+</div> 
+//se nao tivesse essa div não daria pra colocar mais de um componente
+)
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,36 +3,10 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//import ComParametros from './components/layout/ComParametros.jsx'
-import Primeiro from './components/layout/Primeiro.jsx' //importando o componente primeiro para renderizar
-//import ComFilhos from './components/layout/ComFilhos'
-import Card from './components/layout/Card.jsx'
-
 ReactDOM.render( 
 
 
-<div>
-    <Card titulo="exercicio x">
-        <Primeiro />    
-    </Card>
-{/*
-    <ComFilhos>
-        <ul>
-            <li>frase 1</li>
-            <li>frase 2</li>
-            <li>frase 3</li>
-        </ul>
-</ComFilhos>
-
-    <Primeiro />
-    <ComParametros titulo="esse é o titulo" subtitulo="esse é o subtitulo" />
-    <ComParametros titulo="opa" subtitulo="epa" />
-    //titulo e subtitulo sao parametros dos componentes
-*/
-}
-</div> 
-//se nao tivesse essa div não daria pra colocar mais de um componente
     
-,
+
     document.getElementById('root')
 )
