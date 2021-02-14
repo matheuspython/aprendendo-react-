aca009d2f762e8670af9ea1528eb8045212be333
matheus souza <ruivorobado@gmail.com>
2021-02-13 23:28:57 -0300

    exemplo de childreen adicionado

---
+++ fundamentos-react/src/components/ComFilhos.jsx
@@ -0,0 +1,10 @@
+import React from  'react'
+
+export default props =>
+  <div>
+      <h2>Os filhos:</h2>
+      
+      <div>
+        {props.children}
+      </div>
+  </div>
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -4,12 +4,24 @@ import ReactDOM from 'react-dom'
 
 import ComParametros from './components/ComParametros.jsx'
 import Primeiro from './components/Primeiro.jsx' //importando o componente primeiro para renderizar
-
+import ComFilhos from './components/ComFilhos'
 ReactDOM.render( 
     <div>
-        <Primeiro />
-        <ComParametros titulo="esse é o titulo" subtitulo="esse é o subtitulo" />
-        <ComParametros titulo="opa" subtitulo="epa" />
+        <ComFilhos>
+            <ul>
+                <li>frase 1</li>
+                <li>frase 2</li>
+                <li>frase 3</li>
+            </ul>
+        </ComFilhos>
+{
+/*
+    <Primeiro />
+    <ComParametros titulo="esse é o titulo" subtitulo="esse é o subtitulo" />
+    <ComParametros titulo="opa" subtitulo="epa" />
+*/
+}
+    
     </div>//se nao tivesse essa div não daria pra colocar mais de um componente
     //titulo e subtitulo sao parametros dos componentes
 ,
