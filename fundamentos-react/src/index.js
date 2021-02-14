5d6fe617aeacbaaad2078f12f1f9fdd24014b234
matheus souza <ruivorobado@gmail.com>
2021-02-13 21:37:29 -0300

    adicionando dois componentes em uma div

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,9 +2,14 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import ComParametros from './components/ComParametros.jsx'
 import Primeiro from './components/Primeiro.jsx' //importando o componente primeiro para renderizar
 
 ReactDOM.render( 
-    <Primeiro></Primeiro>,
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametros></ComParametros>
+    </div>//se nao tivesse essa div não daria pra colocar mais de um componente
+,
     document.getElementById('root')
 )
